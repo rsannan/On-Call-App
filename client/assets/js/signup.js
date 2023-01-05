35c70117f907c148b50ea0ca2d7ff1f9f50e7d88
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-01-05 10:59:05 -0800

    Finished index.html

--- client/assets/js/signup.js
+++ client/assets/js/signup.js
@@ -1,3 +1,11 @@
 document.querySelector('.img__btn').addEventListener('click', function() {
     document.querySelector('.cont').classList.toggle('s--signup');
   });
+
+  $( "#signupbtn" ).on( "click", function() {
+    window.alert( "<p> was clicked" );
+});
+
+$( "#signinbtn" ).on( "click", function() {
+  window.alert( "<p> was clicked" );
+});
