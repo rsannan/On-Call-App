05d34bc36b94d770c0943592068c6ee375fc5267
Isaac Takiy <kanytakiy@gmail.com>
2023-01-10 15:53:51 +0000

    Modify Intialization Script

--- app_init.sh
+++ app_init.sh
@@ -5,11 +5,9 @@
 source .appenv/bin/activate
 gunicorn --bind 0.0.0.0:5000 --workers 3 -m 007 wsgi:app
 
-if [ $? -eq 0 ]
-then
-	#start the background work
-	rq worker -u redis://127.0.0.1:6379
-else
-	exit 1
-fi
+#terminate all running background workders
+pkill rq
+
+#start the background workers
+rq worker -u redis://127.0.0.1:6379
 
