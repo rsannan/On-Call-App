7defe1d477b12bfa2ce6dbb4ac7a27c82f380769
Isaac Takiy <kanytakiy@gmail.com>
2023-01-10 15:45:14 +0000

    Modify Intialization Script

--- app_init.sh
+++ app_init.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 #this script starts the app
 
+#Initializa and run the flask app
 source .appenv/bin/activate
-flask run
+gunicorn --bind 0.0.0.0:5000 --workers 3 -m 007 wsgi:app
+
+if [ $? -eq 0 ]
+then
+	#start the background work
+	rq worker -u redis://127.0.0.1:6379
+else
+	exit 1
+fi
+
