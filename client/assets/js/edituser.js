f3038469464b28566e5e351272d2206c84f0d982
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-01-11 02:54:06 +0000

    fix edituser page

--- client/assets/js/edituser.js
+++ client/assets/js/edituser.js
@@ -1,23 +1,48 @@
-$( document ).ready(function() {
-    var token = sessionStorage.token;
-    var token1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY3MjkyNzgyMSwianRpIjoiNmQxNzg0NjUtOGIyOC00MTM0LWE0ZjctNzUyZGFiNmYzYjk3IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNjcyOTI3ODIxfQ.ctvSSie9E_xrKN921YmzUtNnE-hp8uxpRDCHGXnfZFs';
-    $.ajax({
-        url: 'http://3.239.72.122:5000/api/checks',
-        type: 'GET',
-        dataType: 'json',
-        headers: {"Authorization": 'Bearer '+token1},
-        success: function(response){
-            console.log(response)
-            $('edfirstname').text('New Header!!!'),
-            $('edlastname').text('New Header!!!'),
-            $('edphone').text('New Header!!!'),
-            $('edemail').text('New Header!!!');
-        },
-        error: function(err) {
-            console.log(err);
-            console.log('Bearer '+token1);
-              }
-          
-    });
-  });
+function getuser() {
+    var userid = 1;
+    var url = 'http://alxtakiy.tech/api/users/' + userid
+    axios({
+        method: 'get',
+        url: url
+    })
+        .then(res => changeout(res))
+        .catch(err => console.error(err));
+  };
 
+  window.addEventListener('load', getuser);
+
+  function changeout(res){
+    // check for user idnumber and match with users data
+    // 
+    let arr = res.data;
+    let name = arr.firstname+ " " + arr.lastname;
+     $('#diname').text(name);
+     $('#diemail').text(arr.email);
+     $('#edfirstname').val(arr.firstname);
+     $('#edlastname').val(arr.lastname);
+     $('#edphone').val(arr.phone);
+     $('#edemail').val(arr.email)
+     
+
+
+};
+
+function updateuser(){
+    var firstname = $('#edfirstname').val();
+	var lastname = $('#edlastname').val();
+		var email 	 = $('#edemail').val();
+		var phone = $('#edphone').val();
+    var data = {firstname: firstname,
+        lastname: lastname,
+        email: email,
+        phone: phone}
+    axios.put('http://alxtakiy.tech/api/users/1', data
+        
+    )
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+
+        document.getElementById('savebtn').addEventListener('click', getuser);
+};
+
+document.getElementById('savebtn').addEventListener('click', updateuser);
