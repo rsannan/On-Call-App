d6ea195e4f886845459a8d8de81f34e833db94b5
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-01-11 10:59:40 +0000

    Add addcheck.js

---
+++ client/assets/js/addcheck.js
@@ -0,0 +1,45 @@
+function getuser() {
+    var userid = 1;
+    var url = 'http://alxtakiy.tech/api/users/' + userid
+    axios({
+        method: 'get',
+        url: url
+    })
+        .then(res => changeout(res))
+        .catch(err => console.error(err));
+  };
+
+
+  function changeout(res){
+    // check for user idnumber and match with users data
+    // 
+    let arr = res.data;
+    let name = arr.firstname+ " " + arr.lastname;
+     $('#acname').text(name);
+     $('#acemail').text(arr.email);
+
+};
+
+function postcheck() {
+    var userid = 1;
+    var url = 'http://alxtakiy.tech/api/checks/'
+    var title = $('#actitle').val();
+    var acurl = $('#acurl').val();
+    var method = $('#acmethod').val();
+    var code = $('#accode').val();
+    var data = {
+        title: title,
+    url: acurl,
+    method_id: method,
+    status_code: code
+    }
+    axios({
+        method: 'post',
+        url: url,
+        data: data
+    })
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+  };
+  window.addEventListener('load', getuser);
+  document.getElementById('acbtn').addEventListener('click', postcheck);
