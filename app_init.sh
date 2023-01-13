6543fadd57504a5ca68f10aad24072c12557fe66
Isaac-web <kanytakiy@gmail.com>
2023-01-13 09:59:24 +0000

    Update app_init.sh

--- app_init.sh
+++ app_init.sh
@@ -6,8 +6,8 @@ source .appenv/bin/activate
 
 
 #terminate all running background workders
-pkill rq
-pkill gunicorn
+sudo pkill rq
+sudo pkill gunicorn
 
 #Initialize and run the flask app
 gunicorn --bind 0.0.0.0:5000 --workers 3 -m 007 wsgi:app & disown
